0dad51484a7c6cce0fc79da431e5412a133a605a
Buni00 <bruno_hrf@outlook.com>
2023-04-21 00:32:13 +0100

    Add GetName and TakeDamage methods

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -13,6 +13,23 @@ namespace GameSix
             shield = 0;
         }
 
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = - shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
     }
 
 }
