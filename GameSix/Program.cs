9d500dc5055c36144aff6c787e6ba44285dd5834
Buni00 <bruno_hrf@outlook.com>
2023-04-21 00:59:45 +0100

    Add print enemie names

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -9,7 +9,21 @@ namespace GameSix
             Console.Write("Number of enemies: ");
             int nEnemies = Convert.ToInt32(Console.ReadLine());
 
-            
+            Foe[] enemies = new Foe[nEnemies];
+
+            for (int i = 0; i< nEnemies; i++)
+            {
+                Console.Write($"Name of Enemies {i+1}: ");
+                string name = Console.ReadLine();
+
+                enemies[i] = new Foe(name);
+
+            }
+
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                Console.WriteLine($"Enemie {i+1} = {enemies[i].GetName()}");
+            }
             
         }
     }
