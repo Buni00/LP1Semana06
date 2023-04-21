fb9b1ff89c071609c4f0c9cb0b3336efb980478b
Buni00 <bruno_hrf@outlook.com>
2023-04-21 14:13:27 +0100

    Add GameLevel constructor

--- GameSixFriday/GameLevel.cs
+++ GameSixFriday/GameLevel.cs
@@ -1,14 +1,46 @@
+using System;
+
 namespace GameSixFriday
-{
+{       
+    public enum Difficulty
+    {
+        Easy,
+        Normal,
+        Hard
+    }
     public class GameLevel
     {
-        public enum Difficulty
+        
+
+        private int nRooms;
+        private Difficulty difficulty;
+        private Foe[] foes;
+
+        public GameLevel(int nRooms, Difficulty difficulty)
         {
-            Easy,
-            Normal,
-            Hard
+            this.nRooms = nRooms;
+            this.difficulty = difficulty;
+            this.foes = new Foe[nRooms];
         }
 
+        public void SetFoeInRoom(int roomIndex, Foe foe)
+        {
+            if (roomIndex < nRooms)
+            {
+                if ( foes[roomIndex]== null)
+                {
+                    foes[roomIndex] = foe;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Only one foe per Room, no overbooking here");
+            }
+        }
+
+
         
+
+
     }
 }
